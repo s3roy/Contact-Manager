b32d17fd6861e2e1d5a0dd78d9ad25d36e83cc47
Souvik <souvikroy1999ab@gmail.com>
2023-08-29 21:46:09 +0530

    added the controller layer

---
+++ controllers/contactController.js
@@ -0,0 +1,42 @@
+//@desc Get all contacts
+//@route Get /api/contacts
+//@access public
+const getContacts = (req, res) => {
+  res.status(200).json({ message: 'Get all contacts' });
+};
+
+//@desc Create new contact
+//@route POST /api/contacts
+//@access public
+const createContact = (req, res) => {
+  res.status(201).json({ message: 'Create contact' });
+};
+
+//@desc Get  contact
+//@route Get /api/contacts/:id
+//@access public
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+};
+
+//@desc Update contact
+//@route PUT /api/contacts/:id
+//@access public
+const updateContact = (req, res) => {
+  res.status(201).json({ message: `Update contact for ${req.params.id}` });
+};
+
+//@desc Delete contact
+//@route DELETE /api/contacts/:id
+//@access public
+const deleteContact = (req, res) => {
+  res.status(201).json({ message: `Delete contact for ${req.params.id}` });
+};
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
