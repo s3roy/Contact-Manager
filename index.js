351af996fd0c847dc110904419188e758eb8f92b
Souvik <souvikroy1999ab@gmail.com>
2023-08-30 13:46:53 +0530

    connected express to mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
